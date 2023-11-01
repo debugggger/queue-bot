8b971253ba1b32a3c01214d037282df933894cc0
Artas Shozhat <shineartsh@mail.ru>
2023-11-02 00:44:24 +0300

    Add new config utils with accessing token

--- queueBot.py
+++ queueBot.py
@@ -1,12 +1,14 @@
 import telebot
 from telebot import types
 import time
+from dotenv import load_dotenv
+import os
+import psycopg2
 
-with open('token.txt') as file:
-    lines = [line.rstrip() for line in file]
-    token = lines[0]
 
-bot=telebot.TeleBot(token)
+load_dotenv()
+bot = telebot.TeleBot(os.getenv('TOKEN'))
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -14,13 +16,17 @@ def start_message(message):
     button1 = types.InlineKeyboardButton("Возможности", callback_data="possibility")
     button2 = types.InlineKeyboardButton("Команды", callback_data="commands")
     markup.row(button1, button2)
-    bot.send_message(message.chat.id,"Привет, я бот для составления очередей. \nТы можешь воспользоваться следующими командами, что бы более подробно узнать что я умею:", reply_markup=markup)
+    bot.send_message(message.chat.id,
+                     "Привет, я бот для составления очередей. \nТы можешь воспользоваться следующими командами, что бы более подробно узнать что я умею:",
+                     reply_markup=markup)
+
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
     commandsList(message)
 
-@bot.callback_query_handler(func = lambda callback: True)
+
+@bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     if callback.data == "com_member":
         bot.send_message(callback.message.chat.id, "добавление в БД")
@@ -34,11 +40,13 @@ def callback_message(callback):
     if callback.data == "commands":
         commandsList(callback.message)
 
+
 @bot.message_handler(content_types=['left_chat_member'])
 def handle_left_chat_member(message):
     user_id = message.left_chat_member.id
     chat_id = message.chat.id
     bot.send_message(chat_id, f"Пользователь с ID {user_id} покинул чат.")
+
 
 def commandsList(message):
     markup = types.InlineKeyboardMarkup()
@@ -46,5 +54,6 @@ def commandsList(message):
     com2 = types.InlineKeyboardButton("/create", callback_data="com_create")
     markup.row(com1, com2)
     bot.send_message(message.chat.id, "можно воспользоваться следующими командами:", reply_markup=markup)
+
 
 bot.infinity_polling()
