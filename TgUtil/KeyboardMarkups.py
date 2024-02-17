677b0f44617de95d16e522be02d32b1d7dd2f636
Artem Vyazemskiy <artvyz17@yandex.ru>
2024-02-17 14:54:49 +0300

    Интеграционные тесты для UserHandlers

---
+++ TgUtil/KeyboardMarkups.py
@@ -0,0 +1,4 @@
+﻿from telebot import types
+
+EnterCancel = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, selective=True).add("Ввод", "❌ Отмена")
+Remove = types.ReplyKeyboardRemove(selective=True)
